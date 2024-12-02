4c37c0315445dd765a2a6be55f2ea80d3654b625
SergeyForsh <seforsh@gmail.com>
2024-12-02 20:56:32 +0300

    Update OP_04_def.py

--- OP_04_def.py
+++ OP_04_def.py
@@ -21,3 +21,20 @@ print(f"Сумма чисел от 10 до 15: {result}")  # Вывод: 15
 # 4. Результат возвращается и выводится на экран.
 
 # Вы можете протестировать функцию с различными значениями `start` и `end`!
+
+print()
+
+def sum_range(start, end):
+    summ = 0
+    if start > end:
+        print("Неверные аргументы!")
+        return summ
+
+    for i in range(start,end+1):
+        summ += i
+    return summ
+
+
+start = int(input("Введите начальное число: "))
+end = int(input("Введите конечное число: "))
+print("Сумма чисел от " + str(start) + " до " + str(end) + " равна: " + str(sum_range(start, end)))
